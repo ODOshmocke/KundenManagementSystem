519cbd30bf1ef8b3cdee49b5fc0a5874d05f7ab7
ODOshmocke <oskar.samsung,j5@gmail.com>
2023-03-19 20:47:35 +0100

    Test

--- backend/models.py
+++ backend/models.py
@@ -8,7 +8,7 @@ class email_information(models.Model):
     email_text = models.TextField(null=True)
 
 class email_senden:
-
+    email_zeit = models.PositiveBigIntegerField(null=True)
 
 def __str__(self):
     return self.email_betreff + " " + self.email_zeit
