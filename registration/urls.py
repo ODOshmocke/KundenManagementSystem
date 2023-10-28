95ca9bfd969ea9509392866b1d39b6d57c82784b
ODOshmocke <73067235+ODOshmocke@users.noreply.github.com>
2023-10-28 12:19:13 +0200

    I just commitest API für unterschrift

--- registration/urls.py
+++ registration/urls.py
@@ -13,13 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from register.views import register
 from dashboard.views import dashboard
+from pdfVerarbeitung.views import unterschriftView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("register/", register),
     path("dashboard/", dashboard),
+    path("unterschrift/", unterschriftView)
 ]
