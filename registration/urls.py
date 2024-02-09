8a02c9030d50b742e8d65e7d1122d1f1ed488bc2
ODOshmocke <73067235+ODOshmocke@users.noreply.github.com>
2024-02-09 19:45:10 +0100

    Dashboard Unterschrift des Leistungsträgers frontend

--- registration/urls.py
+++ registration/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
@@ -27,6 +27,9 @@ from pdfVerarbeitung.views import unterschriftsBestaetigungView
 
 
 urlpatterns = [
+    #authentification
+    path("accounts/", include("django.contrib.auth.urls")),
+
     path('admin/', admin.site.urls),
     path("register/", register),
     path("register/<str:kundenDaten>", register),
